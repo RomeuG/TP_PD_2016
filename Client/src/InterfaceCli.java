90d4b748a85ffb9c4084d2248c0269bd3cdf6f23
João Marques <joaohrik@gmail.com>
2016-12-05 23:32:21 +0000

    added interface - Model

---
+++ Client/src/InterfaceCli.java
@@ -0,0 +1,15 @@
+public interface InterfaceCli 
+{
+    // métodos do Modelo
+    public void register();
+    public void login();
+    public void logout();
+    public void copyFile();
+    public void moveFile();
+    public void changeWorkingDirectory();
+    public void getWorkingDirContent();
+    public void getWorkingDirPath();
+    public void getFileContent();
+    public void removeFile();
+    public void makeDir();
+}
